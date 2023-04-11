a31d9b53e5b1507e882339e6755bd23ffdc6581e
RickyAS <austin251998@gmail.com>
2023-04-11 16:44:00 +0700

    Initial Commit

---
+++ SimpleTestTests/SimpleTestTests.swift
@@ -0,0 +1,27 @@
+//
+//  SimpleTestTests.swift
+//  SimpleTestTests
+//
+//  Created by Ricky Austin on 10/04/23.
+//
+
+import XCTest
+@testable import SimpleTest
+
+final class SimpleTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        let a = 0
+        XCTAssertEqual(a, 0)
+    }
+
+
+}
